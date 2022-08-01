65782888ccfbf2064e780419caa153c09c9742e4
JulSarmiento <jasa1999@hotmail.com>
2022-08-01 16:54:34 -0500

    grade badge functional

--- src/components/Result/Index.jsx
+++ src/components/Result/Index.jsx
@@ -1,26 +1,58 @@
-import React from "react"
-import "./index.css"
+import React from "react";
+let classToAdd;
 
-export default function Result(){
-  return(
+export default function Result({values}) {
+ 
+  const {studentName, className, grade, teacherName} = values
 
+  const resultText = (grade) => {
+    let gradeMod = parseInt(grade)
+
+    if(grade >=0  && grade <= 3) {
+      classToAdd = 'hexagon-color-red'
+      return 'Muy deficiente'
+    }
+    if( gradeMod <= 5){
+      classToAdd = 'hexagon-color-orange '
+      return 'Insuficiente'
+
+    } else if( gradeMod <= 6){
+      classToAdd = 'hexagon-color-yellow'
+      return 'Suficiente'
+
+    } else if( gradeMod <= 7){
+      classToAdd = 'hexagon-color-green-yellow'
+      return 'Bien'
+
+    } else if( gradeMod <= 9){
+      classToAdd = 'hexagon-color-blue-green'
+      return 'Notable'
+
+    } else if(gradeMod === 10) {
+      classToAdd = 'hexagon-color-green'
+      return 'Sobresaliente'
+
+    } 
+  }
+
+
+  return (
     <div className="result-box">
-      <p className="student-name">Nombre del Alumno</p>
+        <p className="student-name">{studentName}</p>
 
-      <div className="number-box">
-        <div className="hexagon">
-          <p className="grade">3</p>
+        <div className="number-box">
+          
+          <div className={`hexagon ${classToAdd}`}>
+            <p className="grade">{grade}</p>
+          </div>
+
+          <div className="result-text-box">
+            <p>{className}</p>
+            <p className="result-text">{!grade ?  " " : resultText(grade)}</p>
+          </div>
         </div>
 
-        <div className="result-text-box">
-          <p>Materia cursada</p>
-          <p className="result-text">Muy deficiente</p>
-        </div>
-      </div>
-
-      <p className="teacher-name">Docente a cargo</p>
+        <p className="teacher-name">{!teacherName ? " " :  <strong> Docente {teacherName}</strong>}</p>
     </div>
-
   )
-  
 }
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,17 +1,88 @@
 import React from "react"
-import Form from "../components/Form/Index"
-import Result from "../components/Result/Index"
+import { useState } from "react";
+import Result from "../components/Result/Index";
 import "./main.css"
 
 export default function Main() {
- 
+
+  const [values, setValues] = useState({});
+
+  const handlerOnChange = (e) => {
+    setValues({ ...values, [e.target.name] : e.target.value })
+  }
+
   return (
     <div className="wrapper">
+
+      <h1>Notas Escolares</h1>
+
       <div className="content-box">
-        <Form /> 
-        <Result /> 
-      </div>
-    </div>
+
+        {/* form */}
+
+        <div className="form-box">
+
+          <h2 className="form-title">Ingrese la nota</h2>
+
+          <form className="form" action="">
+
+            <div>
+              <label htmlFor="student-name">Nombre del estudiante: </label>
+              <input 
+                type="text" 
+                name="studentName" 
+                id="studentName" 
+                onChange={handlerOnChange}
+              />
+            </div>
+
+            <div>
+              <label  htmlFor="class-name">Materia cursada: </label>
+              <input 
+                type="text"
+                name="className" 
+                id="className" 
+                onChange={handlerOnChange}
+              />
+            </div>
+
+            <div>
+              <label  htmlFor="grade">Calificacion final: </label>
+              <input 
+                type="number" 
+                name="grade" 
+                id="grade"
+                min={0}
+                max={10}
+                onChange={handlerOnChange} 
+              />
+            </div>
+
+            <div>
+              <label  htmlFor="teacher-name">Nombre del docente: </label>
+              <input 
+                type="text" 
+                name="teacherName" 
+                id="teacherName"
+                onChange={handlerOnChange} 
+              />
+            </div>
+            
+            <div className="btn-container">
+              <button type="submit">Ingresar</button>
+              <button type="clear">clear</button>
+            </div>
+          </form>
+
+        </div>
+        
+
+        <Result values={values}/>
+        {console.log(values)}
+
+      </div> 
+
+    </div> 
 
   )
 }
